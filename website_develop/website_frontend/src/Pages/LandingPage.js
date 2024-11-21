23b55751256b5939fa20cc96df020765a53972a9
Lianrui Geng <lorrybruce007@gmail.com>
2024-11-20 20:47:10 -0700

    Update LandingPage.js

--- website_develop/website_frontend/src/Pages/LandingPage.js
+++ website_develop/website_frontend/src/Pages/LandingPage.js
@@ -999,6 +999,10 @@ function LandingPage() {
                 Get Started Now
               </StyledButton>
               <StyledButton
+                component="a"
+                href="https://drive.google.com/file/d/1HcU6J3xXaRsxXgUa6oHf1iB3TB1C52dh/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
                 variant="outlined"
                 size="large"
                 startIcon={<DownloadIcon />}
